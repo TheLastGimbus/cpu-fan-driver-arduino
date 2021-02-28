f8a58f67f35ef41e250d910c8156c38a56c28d08
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-02-28 13:28:25 +0100

    The *code*

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include <Arduino.h>
+
+#define FAN_PIN 13
+#define CYCLE_MS 1000
+
+byte currentVal = 127;
+
+void slowPWM(byte pin, byte value) {
+    int on = float(CYCLE_MS) * (value / 255.0);
+    int off = CYCLE_MS - on;
+    digitalWrite(pin, 1);
+    delay(on);
+    digitalWrite(pin, 0);
+    delay(off);
+}
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    pinMode(FAN_PIN, OUTPUT);
 }
 
 void loop() {
-// write your code here
+    if (Serial.available()) {
+        currentVal = Serial.readStringUntil('\n').toInt();
+        if (currentVal > 0 && currentVal < 255) {
+            Serial.println("OK");
+        } else {
+            Serial.println("KO");
+        }
+    }
+    slowPWM(FAN_PIN, currentVal);
 }
